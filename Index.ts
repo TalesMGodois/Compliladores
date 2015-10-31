dc04c54171b2ef74412c653a385c301a4a69e1af
TalesMGodois <tales.godois@gmail.com>
2015-10-31 12:52:40 -0200

    Inserindo alteracoes e testes no Index.ts

--- Index.ts
+++ Index.ts
@@ -1,3 +1,12 @@
 /**
  * Created by tales on 29/10/15.
  */
+import gulp = require("./node_modules/gulp");
+import FileFilter = require('./tsLibs/FileFilter');
+
+var ff = new FileFilter();
+
+ff.importFile('Txts/strings.txt');
+ff.setFileArray();
+
+
