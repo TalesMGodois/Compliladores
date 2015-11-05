01c01559b1506b5d29eedfbd84de2c82517d85eb
TalesMGodois <tales.godois@gmail.com>
2015-11-05 10:28:25 -0200

    Definicoes de nomes sendo inseridas

---
+++ Defines.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by tales on 29/10/15.
+ */
+var Defines = {
+    UTF8:'utf-8'
+}
+
+export = Defines;
---
+++ Modules.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by tales on 29/10/15.
+ */
+var Modules = {
+    Interface:{
+        IFile: './signature/IFile'
+    }
+}
+
+export  = Modules;
